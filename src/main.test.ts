1d0d62682cd65b3c3e7f5b6be7cc45362b30e1ae
JordanMaduro <jordanmaduro@gmail.com>
2021-10-16 15:45:13 +0000

    Add test for foo and delayMillis

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,9 +1,39 @@
-import { greet } from './main'
+import { delayMillis, foo, greet } from "./main";
 
-test('the data is peanut butter', () => {
-  expect(1).toBe(1)
+afterEach(() => {
+  jest.useRealTimers();
 });
 
-test('greeting', () => {
-  expect(greet('Foo')).toBe('Hello Foo')
+test("the data is peanut butter", () => {
+  expect(1).toBe(1);
 });
+
+test("greeting", () => {
+  expect(greet("Foo")).toBe("Hello Foo");
+});
+
+test("delayMillis", (done) => {
+  jest.useFakeTimers();
+  const mockFn = jest.fn();
+
+  delayMillis(1000)
+    .then(mockFn)
+    .then(() => expect(mockFn).toBeCalledTimes(1))
+    .then(done);
+
+  expect(mockFn).not.toBeCalled();
+  jest.advanceTimersByTime(1001);
+});
+
+test("foo", done => {
+  jest.spyOn(global.console, 'log').mockImplementation()
+  jest.useFakeTimers();
+  const mockFn = jest.fn();
+
+  foo().then(value => {
+    expect(value).toBe(true)
+  }).finally(done)
+ 
+  expect(mockFn).not.toBeCalled();
+  jest.advanceTimersByTime(1001);
+})
